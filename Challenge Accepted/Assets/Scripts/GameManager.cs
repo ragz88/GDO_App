f8dca3a566493601a51892f5a8862e4f6ec6bb75
Erik Prinz <erik88d3@gmail.com>
2019-03-24 11:38:38 +0200

    Started Settings scene

---
+++ Challenge Accepted/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool dummyTeam = false;
+
+    public bool[] redTeamNodes;
+    public bool[] blueTeamNodes;
+
+    enum CurrentPage
+    {
+        Chal1,
+        Chal2,
+        Chal3,
+        Elimination,
+        Settings
+    }
+
+    CurrentPage currentPage = CurrentPage.Settings;
+
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        redTeamNodes  = new bool[5];
+        blueTeamNodes = new bool[5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Challenge Accepted/Assets/Scripts/Password.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Password : MonoBehaviour
+{
+
+    public int passcode = 1975;
+    public InputField field;
+
+    public GameObject[] lockObjects;
+    public GameObject[] objectsToHide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < objectsToHide.Length; i++)
+        {
+            objectsToHide[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (field.text == passcode.ToString())
+        {
+            for (int i = 0; i < objectsToHide.Length; i++)
+            {
+                objectsToHide[i].SetActive(true);
+            }
+
+            for (int i = 0; i < lockObjects.Length; i++)
+            {
+                lockObjects[i].SetActive(false);
+            }
+        }
+    }
+}

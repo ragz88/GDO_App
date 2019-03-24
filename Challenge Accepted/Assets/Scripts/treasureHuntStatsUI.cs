b91fdd4419ef666088f4cef9b468722f3ed4213e
Roddymal <41775102+Roddymal@users.noreply.github.com>
2019-03-24 21:52:55 +0200

    Event 1 almost done

---
+++ Challenge Accepted/Assets/Scripts/treasureHuntStatsUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treasureHuntStatsUI : MonoBehaviour
+{
+    public int blipNumber;
+    public bool isDuck = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(isDuck)
+        {
+            if (GameManager.instance.challenge1DuckScore >= blipNumber)
+            {
+                this.gameObject.SetActive(true);
+            }
+            else
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+      else
+        {
+            if (GameManager.instance.challenge1CockScore >= blipNumber)
+            {
+                this.gameObject.SetActive(true);
+            }
+            else
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}

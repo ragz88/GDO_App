bc39a725e10f784c6b48ac789784802252215fbc
Roddymal <41775102+Roddymal@users.noreply.github.com>
2019-03-24 21:05:27 +0200

    Fixed some stuff and made progress

---
+++ Challenge Accepted/Assets/Scripts/ButtonSwitcher.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSwitcher : MonoBehaviour
+{
+    public Sprite button1r;
+    public Sprite button2r;
+    public Sprite button1g;
+    public Sprite button2g;
+    bool buttonOne = true;
+    public bool isStatsButton = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwapButtons ()
+    {
+        if(GameManager.instance.duckTheme)
+        {
+            if (buttonOne)
+            {
+                this.GetComponent<Image>().sprite = button2g;
+                buttonOne = false;
+            }
+            else
+            {
+                this.GetComponent<Image>().sprite = button1g;
+                buttonOne = true;
+            }
+        }
+        else
+        {
+            if (buttonOne)
+            {
+                this.GetComponent<Image>().sprite = button2r;
+                buttonOne = false;
+            }
+            else
+            {
+                this.GetComponent<Image>().sprite = button1r;
+                buttonOne = true;
+            }
+        }
+
+        if(isStatsButton)
+        {
+            if(buttonOne)
+            {
+                //show picture and hints 
+            }
+        }
+
+    }
+
+}
--- Challenge Accepted/Assets/Scripts/ThemeSwitchingElement.cs
+++ Challenge Accepted/Assets/Scripts/ThemeSwitchingElement.cs
@@ -1,28 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThemeSwitchingElement : MonoBehaviour
 {
     public Sprite green;
     public Sprite red;
+    public bool isButton = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+         if(isButton)
+        {
+            if (GameManager.instance.duckTheme)
+            {
+                this.GetComponent<Image>().sprite = green;
+            }
+            else
+            {
+                this.GetComponent<Image>().sprite = red;
+            }
+        }
+        else
+        {
+            if (GameManager.instance.duckTheme)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = green;
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().sprite = red;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.instance.duckTheme)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = green;
-        }
-        else
-        {
-            this.GetComponent<SpriteRenderer>().sprite = red;
-        }
+      
 
     }
 }

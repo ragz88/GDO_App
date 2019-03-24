2a823f3d345a9b0273b4efb11ebe92769b0c23aa
Erik Prinz <erik88d3@gmail.com>
2019-03-24 21:10:10 +0200

    Finished Level 3

---
+++ Challenge Accepted/Assets/Scripts/Event3_Controller.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Event3_Controller : MonoBehaviour
+{
+
+    public GameObject startButton;
+    public GameObject stopText;
+    public GameObject statText;
+    public Image timerCircle;
+    public Text cockScoreTxt;
+    public Text duckScoreTxt;
+    public GameObject cockScoreLabel;
+    //public GameObject duckScoreLabel;
+    public Text winLoseTxt;
+    public GameObject incButton;
+
+    public InputField passwordField;
+    public string password = "ProbsDueToSiya";
+
+    bool countingDown = false;
+    bool timeUp = false;
+
+    float currentValue = 15;//180;  //time in seconds
+    float initialValue;
+
+    int cockScore = 0;
+    //int duckScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialValue = currentValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countingDown)
+        {
+            currentValue -= Time.deltaTime;
+            timerCircle.fillAmount = currentValue / initialValue;
+            if (currentValue < 1)
+            {
+                timeUp = true;
+                countingDown = false;
+                passwordField.gameObject.SetActive(true);
+            }
+        }
+
+        if (timeUp)
+        {
+            
+            stopText.SetActive(true);
+            
+            passwordField.gameObject.SetActive(true);
+            incButton.SetActive(false);
+
+            if (cockScore + 2 < 20)
+            {
+                duckScoreTxt.text = (cockScore + 2).ToString();
+            }
+            else if (cockScore + 1 < 20)
+            {
+                duckScoreTxt.text = (cockScore + 1).ToString();
+            }
+            else
+            {
+                duckScoreTxt.text = (cockScore).ToString();
+            }
+
+            if (cockScore >= 20)
+            {
+                winLoseTxt.text = "DRAW!";
+            }
+            else
+            {
+                winLoseTxt.text = "LOSS!";
+            }
+
+            if (passwordField.text == password)
+            {
+                statText.SetActive(true);
+                passwordField.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    public void startEvent()
+    {
+        countingDown = true;
+        incButton.SetActive(true);
+        cockScoreTxt.gameObject.SetActive(true);
+        cockScoreLabel.SetActive(true);
+        startButton.SetActive(false);
+    }
+
+    public void incScore()
+    {
+        cockScore++;
+        cockScoreTxt.text = cockScore.ToString();
+    }
+}
---
+++ Challenge Accepted/Assets/Scripts/ImageColourChanger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageColourChanger : MonoBehaviour
+{
+
+    public enum ColourChangingType
+    {
+        Image,
+        Text
+    }
+
+    public ColourChangingType ChangerType = ColourChangingType.Image;
+
+    public Color red;
+    public Color green;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChangerType == ColourChangingType.Image)
+        {
+            if (GameManager.instance.duckTheme)
+            {
+                this.GetComponent<Image>().color = green;
+            }
+            else
+            {
+                this.GetComponent<Image>().color = red;
+            }
+        }
+        else if (ChangerType == ColourChangingType.Text)
+        {
+            if (GameManager.instance.duckTheme)
+            {
+                this.GetComponent<Text>().color = green;
+            }
+            else
+            {
+                this.GetComponent<Text>().color = red;
+            }
+        }
+    }
+}

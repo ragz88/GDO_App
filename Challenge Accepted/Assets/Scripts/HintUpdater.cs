7a34482a0ddfc6c2b13488b22ed19080335f999a
Roddymal <41775102+Roddymal@users.noreply.github.com>
2019-03-24 22:06:58 +0200

    Almost there

---
+++ Challenge Accepted/Assets/Scripts/HintUpdater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintUpdater : MonoBehaviour
+{
+    public Sprite[] greenHints;
+    public Sprite[] redHints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.instance.duckTheme)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = greenHints[GameManager.instance.challenge1CockScore];
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = redHints[GameManager.instance.challenge1CockScore];
+        }
+    }
+}

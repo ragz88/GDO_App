5abe2045d507f98b9e62fb047dd55416f8c566fe
Roddymal <41775102+Roddymal@users.noreply.github.com>
2019-03-24 16:00:25 +0200

    Ui stuff

---
+++ Challenge Accepted/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.instance.duckTheme)
+        {
+            this.GetComponent<Camera>().backgroundColor = Color.black;
+        }
+        else
+        {
+            this.GetComponent<Camera>().backgroundColor = Color.white;
+        }
+    }
+}

3cb97eca5fd8e205c5d181a0aa3a5ebb5a973a25
Erik Prinz <erik88d3@gmail.com>
2019-03-24 14:33:49 +0200

    FinishedSettings

--- Challenge Accepted/Assets/Scripts/Settings.cs
+++ Challenge Accepted/Assets/Scripts/Settings.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Settings : MonoBehaviour
 {
     public Button dummyBut;
     public Dropdown nameList;
+    public Dropdown challengeList;
+    public Button themeBut;
 
     public Sprite tickedBox;
     public Sprite untickedBox;
@@ -27,9 +30,25 @@ public class Settings : MonoBehaviour
     public void setDummyFalse()
     {
         GameManager.instance.dummyTeam = false;
-        dummyBut.onClick.AddListener(setDummyFalse);
-        dummyBut.onClick.RemoveListener(setDummyTrue);
+        dummyBut.onClick.AddListener(setDummyTrue);
+        dummyBut.onClick.RemoveListener(setDummyFalse);
         dummyBut.image.sprite = untickedBox;
+    }
+
+    public void setThemeDuck()
+    {
+        //GameManager.instance.duckTheme = true;
+        themeBut.onClick.AddListener(setThemeCock);
+        themeBut.onClick.RemoveListener(setThemeDuck);
+        themeBut.image.sprite = tickedBox;
+    }
+
+    public void setThemeCock()
+    {
+        //GameManager.instance.duckTheme = false;
+        themeBut.onClick.AddListener(setThemeDuck);
+        themeBut.onClick.RemoveListener(setThemeCock);
+        themeBut.image.sprite = untickedBox;
     }
 
     public void EliminatePlayer()
@@ -58,6 +77,30 @@ public class Settings : MonoBehaviour
             newData[i] = new Dropdown.OptionData();
             newData[i].text = GameManager.instance.cockNames[i];
             nameList.options.Add(newData[i]);
+        }
+    }
+
+    public void backButton()
+    {
+        switch (challengeList.value)
+        {
+            case 0:
+                GameManager.instance.currentPage = GameManager.CurrentPage.Chal1;
+                SceneManager.LoadScene("Event_1");
+                break;
+            case 1:
+                GameManager.instance.currentPage = GameManager.CurrentPage.Chal2;
+                SceneManager.LoadScene("Event_2");
+                break;
+            case 2:
+                GameManager.instance.currentPage = GameManager.CurrentPage.Chal3;
+                SceneManager.LoadScene("Event_3");
+                break;
+            case 3:
+                GameManager.instance.currentPage = GameManager.CurrentPage.Elimination;
+                SceneManager.LoadScene("Eliminations");
+                break;
+
         }
     }
 }
